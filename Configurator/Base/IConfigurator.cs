e886a50c686a548bb83c4706bd6274bbe6408b73
Anton <41597155+antontidev@users.noreply.github.com>
2021-11-10 16:47:13 +0500

    Update IConfigurator.cs

--- Configurator/Base/IConfigurator.cs
+++ Configurator/Base/IConfigurator.cs
@@ -1,6 +1,20 @@
+using Source.Scripts.Core.StateMachine.States.Base;
+using System.Threading.Tasks;
+
 namespace Source.Scripts.Core.StateMachine.Configurator.Base
 {
-    public interface IConfigurator
+    public interface IConfigurator<TState, TTrigger> where TState : System.Enum where TTrigger : System.Enum
     {
+        IState<TTrigger> State
+        {
+            get;
+        }
+
+        bool HasTransition(TTrigger trigger);
+        TState Transition(TTrigger trigger);
+
+        bool HasReentry(TTrigger trigger);
+
+        Task Reentry();
     }
 }
